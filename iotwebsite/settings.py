712094f2b359b6e002f5943a9b32a2e050d144c3
king <764666894@qq.com>
2018-03-26 20:38:47 +0800

    增加host，测试界面

--- iotwebsite/settings.py
+++ iotwebsite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jt#jl64p!l692xk=5wjalz!-x&=eqf=dsmrpyxx300&vk(-@%k'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [u'10.122.234.95',u'127.0.0.1']
+ALLOWED_HOSTS = [u'0.0.0.0',u'127.0.0.1',u'10.122.234.95',u'123.207.143.140']
 
 
 # Application definition
--- iotwebsite/urls.py
+++ iotwebsite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
-
+from . import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^smoke/',include('smoke.urls',namespace='smoke'))
+    url(r'^smoke/',include('smoke.urls',namespace='smoke')),
+    url(r'^$',views.test)
 ]
